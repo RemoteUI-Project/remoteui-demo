50fd21832e554446903f3a84d5bdf807a911fcc9
dthommes <daniel@remoteui.org>
2017-01-24 18:14:26 +0100

    Initial example for Geolocation Plugin

---
+++ src/main/java/org/remoteui/demo/framework/GeoLocationActivity.java
@@ -0,0 +1,37 @@
+/** 
+* Created on 22.12.2016 
+* 
+* © 2016 Daniel Thommes
+*/
+package org.remoteui.demo.framework;
+
+import org.remoteui.annotation.Activity;
+import org.remoteui.webapp.WebActivity;
+import org.remoteui.webapp.plugin.geolocation.GeoLocation;
+import org.remoteui.webapp.plugin.geolocation.GeolocationService;
+
+/**
+ * 
+ *
+ * @author Daniel Thommes
+ */
+@Activity(id = "framework:geolocation")
+public class GeoLocationActivity extends WebActivity {
+
+	public GeoLocation location = null;
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// getLocation();
+	}
+
+	public void getLocation(boolean useGps) {
+		firePropertyChanged("location");
+		getWebApp().getPlugin(GeolocationService.class).getGeolocation(useGps, 10000, location -> {
+			this.location = location;
+			firePropertyChanged("location");
+		});
+	}
+
+}
